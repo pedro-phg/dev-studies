6f96741a78921863c40b504228b30de89252b97a
pedro-phg <97642183+pedro-phg@users.noreply.github.com>
2022-01-22 16:33:34 -0300

    atualização - slides

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 function autoScroll() {
-    const redSection = document.querySelectorAll('section')
-    console.log(redSection)
-    const items = redSection.querySelectorAll('li')
-    console.log(items)
-    items.forEach(element => {
-        element.scrollIntoView({
+    const redSection = document.querySelector('.slide-list')
+    const redSectionWidth = redSection.scrollWidth * redSection.childNodes - 1;
+    const sectionImage = redSection.querySelector('li')
+    setInterval(() => {
+        // if (redSection.scrollLeft !== redSectionWidth) {
+        redSection.scrollTo({
+            left: sectionImage.scrollWidth,
             behavior: 'smooth',
-            block: 'start'
+            block: 'center'
         })
-    })
+        // } else if (redSection.scrollLeft === redSectionWidth) {
+        //     redSection.scrollTo({
+        //         left: 0,
+        //         behavior: 'smooth',
+        //         block: 'center'
+        //     })
+        // }
+    }, 5000);
 }
 autoScroll()
 
@@ -18,6 +26,9 @@ function smoothScroll(event) {
     const section = document.querySelector(href)
     section.scrollIntoView({
         behavior: 'smooth',
-        block: 'start'
+        block: 'center'
     })
 }
+
+
+
